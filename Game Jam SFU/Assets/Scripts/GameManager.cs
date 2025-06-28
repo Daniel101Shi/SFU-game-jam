d75a9b482dfea4fe25796b439f25cbf0c7e44e66
roycechenn <100627119+roycechenn@users.noreply.github.com>
2025-06-28 14:24:44 -0700

    fixed

--- Game Jam SFU/Assets/Scripts/GameManager.cs
+++ Game Jam SFU/Assets/Scripts/GameManager.cs
@@ -1,10 +1,18 @@
-public GameObject rhythmPromptPrefab;
-public Transform promptParent;
+// Assets/Scripts/GameManager.cs
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
-    public UIManager ui;
+    [Header("Dev1 References")]
+    public PipeSpawner pipeSpawner;
+    public GameObject gameOverUI;
+
+    [Header("Dev2 Placeholder")]
+    public GameObject rhythmPromptPrefab;
+    public Transform promptParent;
 
     void Awake()
     {
@@ -14,10 +22,22 @@ public class GameManager : MonoBehaviour
     public void OnPlayerDeath()
     {
         Debug.Log("Game Over!");
-        // Add game over handling logic here
+        pipeSpawner.StopSpawning();
+        gameOverUI.SetActive(true);
     }
+
     public void OnPipePassed()
     {
         Instantiate(rhythmPromptPrefab, promptParent);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
