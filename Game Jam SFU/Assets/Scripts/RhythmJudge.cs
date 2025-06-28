7fdb04e0f3f6ed0c33aa9a24a628e224b616f5c3
Daniel <daniel101shi@gmail.com>
2025-06-28 13:25:56 -0700

    template for rhythm judge

---
+++ Game Jam SFU/Assets/Scripts/RhythmJudge.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+public static class RhythmJudge{
+
+    public static void EvulateTiming (float distance, RhythmPrompt prompt) {
+
+    }
+    public static void Missed (RhythmPrompt prompt) {
+    }
+
+    private static void Score(string grade, int multiplier) {
+   
+
+    }
+
+}
