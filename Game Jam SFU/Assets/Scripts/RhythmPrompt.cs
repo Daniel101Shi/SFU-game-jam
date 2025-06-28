021275d170bbed77f710141a217d82d8ff43b250
Daniel <daniel101shi@gmail.com>
2025-06-28 11:35:54 -0700

    finsihed create outer circle start function

--- Game Jam SFU/Assets/Scripts/RhythmPrompt.cs
+++ Game Jam SFU/Assets/Scripts/RhythmPrompt.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+    public Image outerCircle;
+    public Image targetCircle;
 
 public class RhythmPrompt : MonoBehaviour {
     void Start() {
-
+        outerCircle.transform.localScale = Vector3.one * 2f;
+        
     }
     
     void Update() {
