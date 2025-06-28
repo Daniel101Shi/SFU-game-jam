65b00dd390e9e809ee1ecc02ee815cfb66b7bcbc
Daniel <daniel101shi@gmail.com>
2025-06-28 11:33:50 -0700

    create template

---
+++ Game Jam SFU/Assets/Scripts/RhythmPrompt.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class RhythmPrompt : MonoBehaviour {
+    void Start() {
+
+    }
+    
+    void Update() {
+
+    }
+
+    void CleanUp() {
+
+    }
+}
